330d4c9b25cae0338b157f6233596c14a9f22709
Dan Radez <dradez@redhat.com>
2021-11-01 09:00:36 -0400

    adding tmux option to ips.sh

--- ips.sh
+++ ips.sh
@@ -2,9 +2,24 @@ source ./vars.sh
 
 echo ""    
 echo ""    
-# print switch IP addresses
-for l in $SPINES $LEAVES; do
-  SWMAC=$(virsh domiflist $l-vqfx | grep network | awk '{print $5}')
-  echo "$l-vqfx: $SWMAC :: $(virsh net-dhcp-leases default | grep $SWMAC | awk '{print $5}' | cut -d / -f 1)"
-done
 
+if [ "$1" == '-t' ]; then
+  declare -a tmux_spines
+  declare -a tmux_leaves
+  for l in $SPINES; do
+    SWMAC=$(virsh domiflist $l-vqfx | grep default | awk '{print $5}')
+    tmux_spines[${l: -1}]=$(virsh net-dhcp-leases default | grep $SWMAC | awk '{print $5}' | cut -d / -f 1)
+  done
+    tmux new-window "tmux split-pane -h \"tmux split-pane -h \\\"ssh root@${tmux_spines[1]}\\\"; tmux select-layout even-horizontal; ssh root@${tmux_spines[0]}\""
+  for l in $LEAVES; do
+    SWMAC=$(virsh domiflist $l-vqfx | grep default | awk '{print $5}')
+    tmux_leaves[${l: -1}]=$(virsh net-dhcp-leases default | grep $SWMAC | awk '{print $5}' | cut -d / -f 1)
+  done
+    tmux new-window "tmux split-pane -h \"tmux split-pane -h \\\"tmux split-pane -h \\\\\\\"ssh root@${tmux_leaves[2]}\\\\\\\"; ssh root@${tmux_leaves[1]}\\\"; tmux select-layout even-horizontal; ssh root@${tmux_leaves[0]}\""
+else
+  # print switch IP addresses
+  for l in $SPINES $LEAVES; do
+    SWMAC=$(virsh domiflist $l-vqfx | grep default | awk '{print $5}')
+    echo "$l-vqfx: $SWMAC :: $(virsh net-dhcp-leases default | grep $SWMAC | awk '{print $5}' | cut -d / -f 1)"
+  done
+fi
